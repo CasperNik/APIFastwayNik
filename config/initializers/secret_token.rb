554193c1e8778725cc499cfa9c394f1e30436eae
Nik Casper <nikt.net@gmail.com>
2013-07-04 11:43:24 +1200

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fastway1::Application.config.secret_token = 'e49363e87674a59bcf407fb003c65142fe48df32606c07b45fe64044648634eaa368b3d4fd63ac9a203304b50f1fd562b54c9858999f58769f2d2a70f324a8f2'
