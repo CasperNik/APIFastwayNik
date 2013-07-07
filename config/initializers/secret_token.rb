f592ac51ecd327912546bb9b02f62ea3ae9dea2b
Nik Casper <nikt.net@gmail.com>
2013-07-08 11:14:23 +1200

    08.07.2013

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Fastway1::Application.config.secret_token = 'e49363e87674a59bcf407fb003c65142fe48df32606c07b45fe64044648634eaa368b3d4fd63ac9a203304b50f1fd562b54c9858999f58769f2d2a70f324a8f2'
+Fastway1::Application.config.secret_key_base = 'adfsafasfsafsfsafsaf'
